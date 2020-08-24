e45c3b130d2b7c34d427710141f58daa09c3ed91
Robert Ancell <robert.ancell@canonical.com>
2020-08-24 12:18:56 +1200

    Handle more snap metadata

--- example/example.dart
+++ example/example.dart
@@ -1,8 +1,47 @@
+import 'dart:math';
 import 'package:snapd/snapd.dart';
 
 void main() async {
   var client = SnapdClient();
+
+  // Get the installed snaps.
   var snaps = await client.snaps();
-  print(snaps);
+
+  // Show basic information about installed snaps
+  snaps.sort((a, b) => a.name.compareTo(b.name));
+  var rows = [
+    ['Name', 'Version', 'Rev', 'Tracking', 'Publisher']
+  ];
+  for (var snap in snaps) {
+    var channel = '-';
+    if (snap.channel != '') {
+      channel = snap.channel;
+    }
+    var publisher = '-';
+    if (snap.publisher != null) {
+      publisher = snap.publisher.username;
+    }
+    rows.add([
+      snap.name,
+      snap.version ?? '-',
+      snap.revision ?? '-',
+      channel,
+      publisher
+    ]);
+  }
+  var columnWidths = [0, 0, 0, 0, 0];
+  for (var row in rows) {
+    for (var i = 0; i < columnWidths.length; i++) {
+      columnWidths[i] = max(columnWidths[i], row[i].length + 1);
+    }
+  }
+  for (var row in rows) {
+    var line = '';
+    for (var i = 0; i < columnWidths.length; i++) {
+      line += row[i].padRight(columnWidths[i]);
+    }
+    print(line);
+  }
+
   client.close();
 }
