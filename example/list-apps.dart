bcf6a79186ef07913cf853cead767b09bd5bfbdf
Robert Ancell <robert.ancell@canonical.com>
2021-03-02 15:56:57 +1300

    Add SnapdClient.apps
    Fixes https://github.com/canonical/snapd.dart/issues/11

---
+++ example/list-apps.dart
@@ -0,0 +1,14 @@
+import 'package:snapd/snapd.dart';
+
+void main() async {
+  var client = SnapdClient();
+  await client.loadAuthorization();
+
+  // Get the installed apps.
+  var apps = await client.apps();
+  for (var app in apps) {
+    print('${app.name}');
+  }
+
+  client.close();
+}
