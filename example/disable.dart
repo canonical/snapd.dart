7917c6d572b15c0d15d6ea4a43bc61217de5616d
Robert Ancell <robert.ancell@canonical.com>
2022-06-08 12:00:51 +1200

    Add SnapdClient.enable/.disable

---
+++ example/disable.dart
@@ -0,0 +1,23 @@
+import 'package:snapd/snapd.dart';
+
+void main(List<String> args) async {
+  if (args.length != 1) {
+    print('Usage: install <snap>');
+    return;
+  }
+  var name = args[0];
+
+  var client = SnapdClient();
+  await client.loadAuthorization();
+  var id = await client.disable(name);
+  while (true) {
+    var change = await client.getChange(id);
+    if (change.ready) {
+      break;
+    }
+
+    await Future.delayed(Duration(milliseconds: 100));
+  }
+
+  client.close();
+}
