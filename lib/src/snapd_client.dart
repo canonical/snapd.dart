d5b2c4cc912677c8ec2e2017849306573ab08d01
Matthew Hagemann <matt.hagemann@canonical.com>
2025-06-19 10:21:40 +0200

    fix: run the formatter

--- lib/src/snapd_client.dart
+++ lib/src/snapd_client.dart
@@ -509,8 +509,8 @@ class SnapdGenerateRecoveryKeyResponse with _$SnapdGenerateRecoveryKeyResponse {
   }) = _SnapdGenerateRecoveryKeyResponse;
 
   factory SnapdGenerateRecoveryKeyResponse.fromJson(
-          Map<String, dynamic> json,
-          ) =>
+    Map<String, dynamic> json,
+  ) =>
       _$SnapdGenerateRecoveryKeyResponseFromJson(json);
 }
 
